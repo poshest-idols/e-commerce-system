4509c697970b5589f200fbaa3ce66b70e7124e54
Frederick Rice <153444506+poshest-idols@users.noreply.github.com>
2024-01-10 14:07:11 +0700

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,11 @@
+public class Product
+{
+    public string Name { get; }
+    public double Price { get; }
+
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
